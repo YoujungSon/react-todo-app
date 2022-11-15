4183ba7c0f5505e2ee9ee8a2db142110881eb645
YoujungSon <andn1026@gmail.com>
2022-11-15 23:02:21 +0900

    #6.18 Enums

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form';
-import { useSetRecoilState } from 'recoil';
-import { toDoState } from '../atom';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, toDoState } from '../atom';
 
 interface IForm {
   toDo: string;
@@ -8,11 +8,11 @@ interface IForm {
 
 function CreateToDo() {
   const setToDos = useSetRecoilState(toDoState);
-
+  const category = useRecoilValue(categoryState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
 
   const handleValid = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [...oldToDos, { text: toDo, id: Date.now(), category: 'TO_DO' }]);
+    setToDos((oldToDos) => [...oldToDos, { text: toDo, id: Date.now(), category }]);
     setValue('toDo', '');
   };
   return (
