bbd07f79d82f2d664336ff9e286fede0b47f5155
YoujungSon <andn1026@gmail.com>
2022-11-15 19:57:40 +0900

    #6.15 Immutability part Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -8,11 +8,9 @@ function ToDo({ text, category, id }: IToDo) {
       currentTarget: { name },
     } = event;
     setToDos((oldToDos) => {
-      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
-      const newToDo = { text, id, category: name };
-      console.log(oldToDo, newToDo);
-      return oldToDos;
+      const targetIndex = oldToDos.findIndex((toDo: IToDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo['category'] };
+      return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
     });
   };
   return (
